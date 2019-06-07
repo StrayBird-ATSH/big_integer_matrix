0b59abffb202d87b7e039b63a6300ea7f659f76f
WANG, Chen <zhanghuilian-98022@163.com>
2019-06-07 13:41:40 +0800

    Removed redundant +=/-= processing

--- matrix.cpp
+++ matrix.cpp
@@ -6,60 +6,50 @@
 #include <list>
 
 matrix::matrix(std::string matrix[]) {
-    for (int j = 0; j < 9; ++j)
-        matrixContent[j / 3][j % 3] = big_integer(matrix[j]);
+    for (int j = 0; j < 9; ++j) matrixContent[j / 3][j % 3] = big_integer(matrix[j]);
 }
 
 const matrix operator+(matrix &left, matrix &right) {
     matrix result;
     for (int i = 0; i < 9; ++i)
-        result.matrixContent[i / 3][i % 3] =
-                left.matrixContent[i / 3][i % 3] + right.matrixContent[i / 3][i % 3];
+        result.matrixContent[i / 3][i % 3] = left.matrixContent[i / 3][i % 3] + right.matrixContent[i / 3][i % 3];
     return result;
 }
 
 const matrix operator-(const matrix &left, const matrix &right) {
     matrix result;
     for (int i = 0; i < 9; ++i)
-        result.matrixContent[i / 3][i % 3] =
-                left.matrixContent[i / 3][i % 3] - right.matrixContent[i / 3][i % 3];
+        result.matrixContent[i / 3][i % 3] = left.matrixContent[i / 3][i % 3] - right.matrixContent[i / 3][i % 3];
     return result;
 }
 
 const matrix operator*(const matrix &left, const matrix &right) {
     matrix result;
-    for (int i = 0; i < 3; ++i) {
+    for (int i = 0; i < 3; ++i)
         for (int j = 0; j < 3; ++j) {
             big_integer element(0);
-            for (int k = 0; k < 3; ++k) {
-                element += left.matrixContent[i][k] * right.matrixContent[k][j];
-            }
+            for (int k = 0; k < 3; ++k) element += left.matrixContent[i][k] * right.matrixContent[k][j];
             result.matrixContent[i][j] = element;
         }
-    }
     return result;
 }
 
 const matrix operator*(const big_integer &bigNum, const matrix &right) {
     matrix result;
-    for (int i = 0; i < 9; ++i)
-        result.matrixContent[i / 3][i % 3] = bigNum * right.matrixContent[i / 3][i % 3];
+    for (int i = 0; i < 9; ++i) result.matrixContent[i / 3][i % 3] = bigNum * right.matrixContent[i / 3][i % 3];
     return result;
 }
 
 const matrix operator~(const matrix &right) {
     matrix result;
-    for (int i = 0; i < 3; ++i)
-        for (int j = 0; j < 3; ++j)
-            result.matrixContent[i][j] = right.matrixContent[j][i];
+    for (int i = 0; i < 3; ++i) for (int j = 0; j < 3; ++j) result.matrixContent[i][j] = right.matrixContent[j][i];
     return result;
 }
 
 matrix &operator+=(matrix &left, const matrix &right) {
     matrix result;
     for (int i = 0; i < 9; ++i)
-        result.matrixContent[i / 3][i % 3] =
-                left.matrixContent[i / 3][i % 3] + right.matrixContent[i / 3][i % 3];
+        result.matrixContent[i / 3][i % 3] = left.matrixContent[i / 3][i % 3] + right.matrixContent[i / 3][i % 3];
     left = result;
     return left;
 }
@@ -67,40 +57,18 @@ matrix &operator+=(matrix &left, const matrix &right) {
 matrix &operator-=(matrix &left, const matrix &right) {
     matrix result;
     for (int i = 0; i < 9; ++i)
-        result.matrixContent[i / 3][i % 3] =
-                left.matrixContent[i / 3][i % 3] - right.matrixContent[i / 3][i % 3];
+        result.matrixContent[i / 3][i % 3] = left.matrixContent[i / 3][i % 3] - right.matrixContent[i / 3][i % 3];
     left = result;
     return left;
 }
 
 std::ostream &operator<<(std::ostream &stream, const matrix &matrix1) {
-    for (auto &line:matrix1.matrixContent)
-        stream << line[0] << " " << line[1] << " " << line[2] << "\n";
+    for (auto &line:matrix1.matrixContent) stream << line[0] << " " << line[1] << " " << line[2] << "\n";
     return stream;
 }
 
 matrix computeExpression(const std::map<std::string, matrix> &map, const std::string &expression) {
-    std::string localExpression = expression;
-    std::list<std::string> subExpressions; //strings that are split by += and -=
-    std::list<std::string> operators;
-    while (expression.find("+=") != std::string::npos || expression.find("-=") != std::string::npos) {
-        int index1 = expression.find("+="), index2 = expression.find("-=");
-        if (((index1 != -1 && index2 != -1) && (index1 < index2)) || index2 == -1) {
-            subExpressions.push_back(localExpression.substr(0, index1));
-            operators.emplace_back("+=");
-            localExpression = localExpression.substr(index1 + 2);
-        } else {
-            subExpressions.push_back(localExpression.substr(0, index2));
-            operators.emplace_back("-=");
-            localExpression = localExpression.substr(index2 + 2);
-        }
-    }
-//    todo: To be changed in the future
-    subExpressions.push_back(localExpression);
-    std::list<matrix> matrixes;
-    for (auto &subExpression:subExpressions)
-        matrixes.push_back(computeExpression1(subExpression, map));
-    return matrixes.back();
+    return computeExpression1(expression, map);
 }
 
 matrix computeExpression1(std::string expression, const std::map<std::string, matrix> &map) {
