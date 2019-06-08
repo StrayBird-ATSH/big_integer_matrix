06765dbaacce041040677eee54df674b09206709
WANG, Chen <zhanghuilian-98022@163.com>
2019-06-08 21:58:00 +0800

    Add comments; Renamed variables

--- matrix.h
+++ matrix.h
@@ -1,13 +1,30 @@
-//
-// Created by zhang on 5/31/2019.
-//
-
 #ifndef BIG_NUM_MATRIX_H
 #define BIG_NUM_MATRIX_H
 
 #include "bigInteger.h"
 #include <map>
 
+/**
+ * The matrix class is designed to represent a 3 by 3 matrix whose
+ * elements are big integers. The big integer implementation relies
+ * on the big integer class in this project. The maximal length of the
+ * big integers depend on the length allowed for the strings on a system
+ * The matrix class support the same operations as the big integers.
+ *
+ * The operators supported by this class are add, subtract and multiply.
+ * However, some other manipulations are currently realized for the sake of
+ * internal usage. The three operators are overloading the system operators
+ * +, -, and * separately.
+ * It should be noted that * operator has two different functions. They are
+ * matrix multiply matrix and big integer multiply matrix separately.
+ *
+ * The constructor of this class allows string array argument only. However, for
+ * the sake of copy constructors, an empty constructor is also designed.
+ *
+ * @author Wang, Chen
+ * @class matrix
+ * @date June 4th, 2019
+ */
 class matrix {
 public:
     matrix() = default;
